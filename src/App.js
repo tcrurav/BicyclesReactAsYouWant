fb262674618769541447de3d7fee103a0837646b
tcrurav <tiburcio.cruz@gmail.com>
2022-04-07 19:25:32 +0100

    second commit of my life

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hola holita carcolita bonita</p>
+        <p>Hey Smuke</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
