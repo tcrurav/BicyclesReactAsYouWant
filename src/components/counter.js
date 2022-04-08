20ce7c147b8e48a177250123ccd95e48acee8f8b
tcrurav <tiburcio.cruz@gmail.com>
2022-04-08 16:56:47 +0100

    Counter

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,11 +1,18 @@
 import { useState } from "react";
 
 
-function Counter(){
+function Counter() {
   const [count, setCount] = useState(0);
 
+  const addOneToCounter = () => {
+    setCount(count + 1);
+  }
+
   return (
-    <p>Counter: {count}</p>
+    <>
+      <p>Counter: {count}</p>
+      <button onClick={addOneToCounter}>Add</button>
+    </>
   );
 }
 
